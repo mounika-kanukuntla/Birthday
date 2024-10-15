7ffa5df38d4bf4ed5e9da1151160a02659ad0e2b
unknown <mounikakanukuntla2002@gmail.com>
2024-10-15 10:32:44 +0530

    happy

---
+++ src/db.jsx
@@ -0,0 +1,39 @@
+let users=[
+    {
+       id:"1",
+        profile:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxrm5YKBpG1E6LFoulNN1JTFFLgggFt8gu4w&s   " ,
+        name:"Shinobu Kocho",
+         age:"28 years"
+
+    },
+    {
+        id:"2",
+        profile:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4wMhZoK4DH-X6T-lZjikKnUWEVjLkOz0O3w&s" ,
+        name:"Zenistu",
+         age:"22 years"
+    },
+    {
+        id:"3",
+        profile:"https://beebom.com/wp-content/uploads/2023/01/Kyojuro-Rengoku-Flame-Hashira.jpg?w=640https://beebom.com/wp-content/uploads/2023/01/Kyojuro-Rengoku-Flame-Hashira.jpg?w=640" ,
+        name:"Rengoku san",
+        age:"42 years" 
+    },
+    {
+        id:"4",
+        profile:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzODLkXJ7EYXeI1qW_9bUnPDpU3eY-rLb_rA&s" ,
+        name:"Kamado Tanjiro",
+         age:"24 years"  
+    },
+    {
+        id:"5",
+        profile:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS249sl8iRsGWrZsTJOjzZlnXSiA7IxLBhGrOWfRV1-pAzGPAYlSa8gckV7CycEpO7W3nM&usqp=CAU" ,
+        name:"Nezukoo",
+         age:"21 years"  
+    },{
+        id:"6",
+        profile:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRl5GfJPrAmYVxC-mIlBsty-VVS4AnLZK4OEULl8iBBn_cxJ0aA3HZk36XwuSu05Rd4sUg&usqp=CAU" ,
+        name:"Mitsuri",
+         age:"22 years"  
+    }
+]
+export default users
